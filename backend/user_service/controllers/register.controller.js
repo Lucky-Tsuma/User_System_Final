fc6301fe7cbc4afbcbf14d0574525e981db27155
Lucky-Tsuma <tsumahmumba@gmail.com>
2021-12-07 17:00:24 +0300

    Validate input using joi

--- backend/user_service/controllers/register.controller.js
+++ backend/user_service/controllers/register.controller.js
@@ -1,8 +1,24 @@
 const db = require('../config');
+const joi = require('joi');
 const { encrypt } = require('../helpers/encryptPassword');
+
+const schema = joi.object().keys({
+    firstname: joi.string().required(),
+    lastname: joi.string().required(),
+    email: joi.string().email().required(),
+    phone: joi.string().min(10).required(),
+    role: joi.string().required(),
+    password: joi.string().pattern(new RegExp('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$'))
+});
 
 module.exports = {
     registerUser: async(req, res) => {
+
+        const { error } = schema.validate(req.body);
+
+        if(error) {
+            return res.json({ success: 0, message: error.details[0].message });
+        }
 
         const { firstname, lastname, email, phone, role } = req.body;
         const password = await encrypt(req.body.password);
